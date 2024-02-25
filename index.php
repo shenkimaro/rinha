a3a90b902982773bd7b6a6f41d44923249cc8e12
ibanez <ibanez.almeida@gmail.com>
2024-02-25 17:13:11 -0300

    Remoção de debug

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
-echo '<pre>';
-print_r($_SERVER);die;
 session_start();
 include_once('./config/requires.php');
-Util::debug($_REQUEST);
 ini_set('display_errors', 'on');
 error_reporting(E_ALL);
 (new Controller())->start();
